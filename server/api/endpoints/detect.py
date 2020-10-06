3bfc17a5a1667b3b656449409ecd4e84559f9890
serhii <meksikann@gmail.com>
2020-10-06 19:41:24 +0300

    on start app event

--- server/api/endpoints/detect.py
+++ server/api/endpoints/detect.py
@@ -14,10 +14,14 @@ good_text = '[CWD-120] I was working on bug fixing due to the ticketяя created
 
 @detect_router.get('/language_check/{text}')
 async def check_language(text):
-    print('received:', text)
     timer.tic()
-    matches_2 = tool.check(text)
+    matches = tool.check(text)
     timer.toc('Text check took:')
-    print(f'This comment contains {len(matches_2)} errors')
 
-    return 'Text is ok'
+    feedback = ''
+
+    for match in matches:
+        print(match.ruleId)
+        feedback += "" + match.ruleId
+
+    return feedback
--- server/main.py
+++ server/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from server.api import router
 
+
+
 app = FastAPI(title='Sentence check server', version='1')
+
+
+@app.on_event('startup')
+async def init_detector():
+    print('Language check initialization started')
+    from server.api.endpoints.detect import check_language
+
+    await check_language('text init')
+    print('Language check initialization finished')
+
+
+
+
 
 # app.add_middleware(myMiddleware)
 app.include_router(router)
